88dcdbbe8e1d0d0dedb3ffe1cbe989163b5f89fc
Tritemis <106695577+Tritemis@users.noreply.github.com>
2022-06-29 18:56:47 +0200

    Create FE-Clothing-Spider.lua

---
+++ FE-Clothing-Spider.lua
@@ -0,0 +1,21 @@
+--Jacket: 
+local thing = game.Players.LocalPlayer.Character
+local originalpos = thing.HumanoidRootPart.CFrame
+thing.HumanoidRootPart.CFrame = CFrame.new(math.huge,math.huge,math.huge)
+
+wait(15)
+thing.RightLowerLeg.RightKnee:Destroy()
+wait(0.5)
+thing.LeftLowerLeg.LeftKnee:Destroy()
+
+wait(0.5)
+thing.UpperTorso.Waist:Destroy()
+wait(0.3)
+
+thing.HumanoidRootPart.CFrame = originalpos
+
+wait(0.1)
+
+wait()
+
+thing.UpperTorso.Anchored=true
