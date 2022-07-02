3c066d50c27a6d73ab29d5710ce24ca50cebac3c
Tritemis <106695577+Tritemis@users.noreply.github.com>
2022-07-02 11:32:07 +0200

    Create FE-HoverBoard-V2.lua

---
+++ FE-HoverBoard-V2.lua
@@ -0,0 +1,8 @@
+-- FE Hoverboard
+--[[ 
+Hats required :
+2646473721 - Roblox Visor https://www.roblox.com/catalog/2646473721/Roblox-Visor
+376526673 - Stylish Aviators https://www.roblox.com/catalog/376526673/Stylish-Aviators
+376527500 - Orange Shades https://www.roblox.com/catalog/376527500/Orange-Shades
+]]
+loadstring(game:GetObjects('rbxassetid://7339698872')[1].Source)()
