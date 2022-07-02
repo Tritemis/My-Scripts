92fb8358c0f581eeddcb251142fc4b3bccd1c9a9
Tritemis <106695577+Tritemis@users.noreply.github.com>
2022-07-02 20:36:00 +0200

    Create FE-Biker-With-Girl-Shortcut.lua

---
+++ FE-Biker-With-Girl-Shortcut.lua
@@ -0,0 +1,29 @@
+-- controls :
+-- press E to ride/toggle
+-- press Q to ride with girl/toggle
+--[[
+--
+https://www.roblox.com/catalog/6470135113/Fan-Hand-Sign-Why-Dont-We-WDW
+--
+https://www.roblox.com/catalog/376527115/Jade-Necklace-with-Shell-Pendant
+--
+https://www.roblox.com/catalog/451220849/Lavender-Updo
+-- 
+https://www.roblox.com/catalog/63690008/Pal-Hair
+-- 
+https://www.roblox.com/catalog/376524487/Blonde-Spiked-Hair
+--
+https://www.roblox.com/catalog/1103003368/Orange-Beanie-with-Black-Hair
+--
+https://www.roblox.com/catalog/48474294/ROBLOX-Girl-Hair
+--
+https://www.roblox.com/catalog/3033908130/International-Fedora-France
+--
+https://www.roblox.com/catalog/62724852/Chestnut-Bun
+
+[If hairs are out of sale, just buy the character of that hair. The character will be linked in description or you can search it by the hair name]
+-- https://www.roblox.com/catalog/9743558381/TJ-Wearable-BMX-Backpack
+
+[ to get this backpack goto https://www.roblox.com/games/9129288160/Tommy-Play game & interact with quest then select BMX bike or watch this video https://youtu.be/MTISKB1AdRU if you're confused]
+]]--
+loadstring(game:HttpGet(('https://github.com/Tritemis/My-Scripts/blob/main/FE-Biker-With-Girl.lua'),true))()
