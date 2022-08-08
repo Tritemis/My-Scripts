db34e408ce357609894a5fd4797fdd3b54b3a886
Tritemis <106695577+Tritemis@users.noreply.github.com>
2022-08-08 12:15:22 +0200

    Update FE-Dino.lua

--- FE-Dino.lua
+++ FE-Dino.lua
@@ -1,3 +1,15 @@
+--[[
+hats:
+https://www.roblox.com/catalog/376524487/Blonde-Spiked-Hair
+https://www.roblox.com/catalog/63690008/Pal-Hair
+https://www.roblox.com/catalog/62234425/Brown-Hair
+https://www.roblox.com/catalog/3302591859/Godzilla-Spine-Backpack
+https://www.roblox.com/catalog/3302593407/Rodans-Head
+https://www.roblox.com/catalog/9661543986/Nike-Shoebox-Costume
+https://www.roblox.com/catalog/62724852/Chestnut-Bun
+https://www.roblox.com/catalog/48474294/ROBLOX-Girl-Hair
+https://www.roblox.com/catalog/451220849/Lavender-Updo
+]]--
 function rmesh(a)
 if not (workspace[game.Players.LocalPlayer.Name][a].Handle:FindFirstChild('Mesh') or workspace[game.Players.LocalPlayer.Name][a].Handle:FindFirstChild('SpecialMesh')) then return end
 old=game.Players.LocalPlayer.Character
